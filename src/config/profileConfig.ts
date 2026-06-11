2bfcd503526b9439e5003c1074a86110dd64190c
zpooi <448859157@qq.com>
2026-06-11 18:14:55 +0800

    初始化项目

---
+++ src/config/profileConfig.ts
@@ -0,0 +1,39 @@
+import type { ProfileConfig } from "../types/config";
+
+// 个人资料配置
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	name: "まつざか ゆき",
+	bio: "世界は大きい、君は行かなければならない",
+	typewriter: {
+		enable: true, // 启用个人简介打字机效果
+		speed: 80, // 打字速度（毫秒）
+	},
+	links: [
+		{
+			name: "Bilibili",
+			icon: "fa7-brands:bilibili",
+			url: "https://space.bilibili.com/701864046",
+		},
+		{
+			name: "Gitee",
+			icon: "mdi:git",
+			url: "https://gitee.com/matsuzakayuki",
+		},
+		{
+			name: "GitHub",
+			icon: "fa7-brands:github",
+			url: "https://github.com/matsuzaka-yuki",
+		},
+		{
+			name: "Codeberg",
+			icon: "simple-icons:codeberg",
+			url: "https://codeberg.org",
+		},
+		{
+			name: "Discord",
+			icon: "fa7-brands:discord",
+			url: "https://discord.gg/MqW6TcQtVM",
+		},
+	],
+};
